6d1fc47642dbc4f69a5e6879b1c2ebe87405cdb8
dageon <93782184+dageon@users.noreply.github.com>
2023-11-16 15:40:57 +0900

    3

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
+void swap(int* x, int* y) {
+	int temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
 void main(void) {
-	int i = 300;
-	
-	int *pi = &i;
-	char *pc = &i;
-	
-	printf("%i, %i, %i\n", i, *pi, *pc);
-	
-	return 0;
+	int a = 3;
+	int b = 5;
+	swap(&a, &b);
+	printf("a:%i, b:%i\n", a, b);
 }
